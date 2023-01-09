28748a2116b7fea56592d2f45814108cf4397437
Mashym Ali <mashymalii@gmail.com>
2023-01-09 22:59:25 +0500

    serving audio left

--- app.py
+++ app.py
@@ -1,29 +1,64 @@
-from flask import Flask, render_template, request
-from werkzeug import secure_filename
+import os
+from flask import Flask, flash, request, redirect, url_for,render_template,send_from_directory
+from werkzeug.utils import secure_filename
+from PyPDF2 import PdfReader
+import pyttsx3
+import uuid
+
+
 
 app = Flask(__name__)
 
-
-app.config['UPLOAD_FOLDER'] = './uploaded_pdf'
-app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
-
-ALLOWED_EXTENSIONS = set(['pdf'])
+ALLOWED_EXTENSIONS = {'pdf'}
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1000 * 1000
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def pdf_to_voice(file,sound):
+    global audio_file
+    reader = PdfReader(file)
+    number_of_pages = len(reader.pages)
+    text = ""
+    for i in range(number_of_pages):
+        page = reader.pages[i]
+        text += page.extract_text() 
+
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[sound].id)
+    audio_file = f'{uuid.uuid1()}.mp3'
+    engine.save_to_file(text, audio_file)
+    engine.runAndWait()
+
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+@app.route('/convert',methods=['GET', 'POST'])
+def convert():
+    if request.method == 'POST':
+        if 'pdf' not in request.files:
+            flash("no file")
+            return redirect(request.url)
+        file = request.files['pdf']
+        if file.filename == '':
+            print('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            sfilename = secure_filename(file.filename)
+            pdf_path = os.path.join('./static/uploadedPDF', sfilename)
+            file.save(pdf_path)
+            print("file saved")
+            #return redirect(url_for('uploaded_file', filename=filename))
 
+        sound = int(request.form.get('chosen_voice'))
+        pdf_to_voice(pdf_path,sound)
+
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
