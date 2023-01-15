f4f7a0bbb77feaaa28611ad588d2efde9979ef1a
Mashym Ali <mashymalii@gmail.com>
2023-01-15 15:35:51 +0500

    serve mp3 files

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask, flash, request, redirect, url_for,render_template,send_from_directory
+from flask import Flask, flash, request, redirect, render_template,after_this_request
 from werkzeug.utils import secure_filename
 from PyPDF2 import PdfReader
 import pyttsx3
 import uuid
+from threading import Timer
 
 
 
@@ -32,6 +33,20 @@ def pdf_to_voice(file,sound):
     engine.save_to_file(text, audio_file)
     engine.runAndWait()
 
+def clean_folder(folder_path):
+    for file in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(e)
+
+def schedule_folder_clean():
+    Timer(120, schedule_folder_clean).start() # schedule the next cleaning in 2 minutes
+    clean_folder("./static/audios")
+
+schedule_folder_clean()
 
 
 @app.route('/')
@@ -40,25 +55,32 @@ def home():
 
 @app.route('/convert',methods=['GET', 'POST'])
 def convert():
+
     if request.method == 'POST':
+
         if 'pdf' not in request.files:
             flash("no file")
-            return redirect(request.url)
+            return render_template("index.html")
+
         file = request.files['pdf']
+
         if file.filename == '':
             print('No selected file')
-            return redirect(request.url)
+            return render_template("index.html")
+
         if file and allowed_file(file.filename):
             sfilename = secure_filename(file.filename)
             pdf_path = os.path.join('./static/uploadedPDF', sfilename)
             file.save(pdf_path)
             print("file saved")
-            #return redirect(url_for('uploaded_file', filename=filename))
-        sound = int(request.form.get('chosen_voice'))
-        pdf_to_voice(pdf_path,sound)
+            sound = int(request.form.get('chosen_voice'))
+            pdf_to_voice(pdf_path,sound)
+            os.remove(pdf_path)
 
-        
-    
+        else:
+            error_msg="Only pdf files are allowed"
+            return render_template("index.html",error_msg=error_msg)
+
     return render_template("audio.html",audio_file=audio_file)
 
 if __name__ == '__main__':
